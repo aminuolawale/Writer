e15e0131595525b1c67610954504fdf4727f7e8f
mohammedaminu <aminuolawalekan@gmail.com>
2023-11-06 11:42:52 +0100

    Refactor writing canvas component

---
+++ app/src/main/java/com/aminuolawale/writer/presentation/writing_canvas/components/WritingCanvasComp.kt
@@ -0,0 +1,48 @@
+package com.aminuolawale.writer.presentation.writing_canvas.components
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
+import com.aminuolawale.writer.domain.model.Line
+
+@Composable
+fun WritingCanvasComp(lines: List<Line>, isErasing:Boolean, onPointerDrag:(Line)->Unit) {
+    Canvas(modifier = Modifier
+        .shadow(elevation = 6.dp)
+        .fillMaxHeight(0.95F)
+        .fillMaxWidth()
+        .padding(10.dp)
+        .background(color = Color.White)
+        .pointerInput(true) {
+            detectDragGestures { change, dragAmount ->
+                change.consume()
+                val line = Line(
+                    start = change.position - dragAmount,
+                    end = change.position,
+                    color = if (isErasing) Color.White else Color.Black,
+                    strokeWidth = if (isErasing) 40.dp else 1.dp
+                )
+                onPointerDrag(line)
+            }
+        }, onDraw = {
+        lines.forEach { line ->
+            drawLine(
+                color = line.color,
+                start = line.start,
+                end = line.end,
+                strokeWidth = line.strokeWidth.toPx(),
+                cap = StrokeCap.Round
+            )
+        }
+    })
+}
--- app/src/main/java/com/aminuolawale/writer/presentation/writing_canvas/components/WritingCanvasScreen.kt
+++ app/src/main/java/com/aminuolawale/writer/presentation/writing_canvas/components/WritingCanvasScreen.kt
@@ -1,13 +1,9 @@
 package com.aminuolawale.writer.presentation.writing_canvas.components
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,23 +11,17 @@ import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.aminuolawale.writer.presentation.home.components.Appbar
 import com.aminuolawale.writer.presentation.writing_canvas.DrawingMode
-import com.aminuolawale.writer.domain.model.Line
 import com.aminuolawale.writer.presentation.writing_canvas.WritingCanvasEvent
 import com.aminuolawale.writer.presentation.writing_canvas.WritingCanvasViewModel
 import kotlinx.coroutines.flow.collectLatest
@@ -42,54 +32,33 @@ fun WritingCanvasScreen(
     navController: NavController,
     viewModel: WritingCanvasViewModel = hiltViewModel()
 ) {
-    val state  = viewModel.state
+    val state = viewModel.state
     val scaffoldState = rememberScaffoldState()
-    LaunchedEffect(key1 =true ){
+    LaunchedEffect(key1 = true) {
         viewModel.eventFlow.collectLatest { event ->
-            when(event) {
+            when (event) {
                 is WritingCanvasViewModel.UiEvent.CanvasSaved -> {
                     scaffoldState.snackbarHostState.showSnackbar(message = "Saved")
                 }
             }
         }
     }
-    Scaffold(scaffoldState= scaffoldState) {
+    Scaffold(scaffoldState = scaffoldState) {
         Column(modifier = Modifier.fillMaxSize()) {
             Appbar()
-            Canvas(modifier = Modifier
-                .shadow(elevation = 6.dp)
-                .fillMaxHeight(0.95F)
-                .fillMaxWidth()
-                .padding(10.dp)
-                .background(color = Color.White)
-                .pointerInput(true) {
-                    detectDragGestures { change, dragAmount ->
-                        change.consume()
-                        val line = Line(
-                            start = change.position - dragAmount,
-                            end = change.position,
-                            color = if (state.value.isErasing) Color.White else Color.Black,
-                            strokeWidth = if (state.value.isErasing) 40.dp else 1.dp
-                        )
-                        viewModel.onEvent(WritingCanvasEvent.Draw(line))
-                    }
-                }, onDraw = {
-                state.value.lines.forEach { line ->
-                    drawLine(
-                        color = line.color,
-                        start = line.start,
-                        end = line.end,
-                        strokeWidth = line.strokeWidth.toPx(),
-                        cap = StrokeCap.Round
-                    )
-                }
-            })
+            WritingCanvasComp(
+                lines = state.value.lines,
+                isErasing = state.value.isErasing
+            ) { line ->
+                viewModel.onEvent(WritingCanvasEvent.Draw(line))
+
+            }
             Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
                 WritingPalette(
                     modifier = Modifier.padding(end = 10.dp),
                     onDrawClick = { viewModel.onEvent(WritingCanvasEvent.ChangeDrawMode(DrawingMode.IsDrawing)) },
                     onEraseClick = { viewModel.onEvent(WritingCanvasEvent.ChangeDrawMode(DrawingMode.IsErasing)) })
-                Button(onClick ={viewModel.onEvent(WritingCanvasEvent.Undo)}) {
+                Button(onClick = { viewModel.onEvent(WritingCanvasEvent.Undo) }) {
                     Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Undo")
                 }
                 Button(onClick = { viewModel.onEvent(WritingCanvasEvent.Redo) }) {
