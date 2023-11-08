59d959f1d9413e6a1890a81744723ef1b1c304c8
mohammedaminu <aminuolawalekan@gmail.com>
2023-11-09 00:19:06 +0100

    Datetime formatting

--- app/src/main/java/com/aminuolawale/writer/presentation/home/components/CanvasItem.kt
+++ app/src/main/java/com/aminuolawale/writer/presentation/home/components/CanvasItem.kt
@@ -24,38 +24,54 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun CanvasItem(title: String, lastUpdated: Long, onClick: () -> Unit, onLongClick:()-> Unit) {
-        Column(
+fun CanvasItem(title: String, lastUpdated: Long, onClick: () -> Unit, onLongClick: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp)
+            .shadow(elevation = 15.dp)
+            .combinedClickable(
+                onClick = onClick, onLongClick = onLongClick
+            )
+    ) {
+        Surface(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(10.dp)
-                .shadow(elevation = 15.dp).combinedClickable  (
-                    onClick=  onClick, onLongClick = onLongClick
-                )
-        ) {
-            Surface(
+                .fillMaxWidth()
+                .height(180.dp), color = Color.Cyan
+        ) {}
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(180.dp), color = Color.Cyan
-            ) {}
-            Surface(modifier = Modifier.fillMaxSize()) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(5.dp)
+                    .padding(5.dp)
+            ) {
+                Text(text = title, fontSize = 20.sp)
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Text(text = title, fontSize = 20.sp)
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Text(text = lastUpdated.toString())
-                        Text(text = lastUpdated.toString())
-                    }
+                    Text(
+                        text = SimpleDateFormat("d/m/y", Locale.getDefault()).format(
+                            Date(
+                                lastUpdated
+                            )
+                        )
+                    )
+                    Text(
+                        text = SimpleDateFormat("hh:mm", Locale.getDefault()).format(
+                            Date(
+                                lastUpdated
+                            )
+                        )
+                    )
                 }
             }
         }
     }
+}
