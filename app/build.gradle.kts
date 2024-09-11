d90a869efebb6d0bd532da315359167e51f4517c
aminuolawale <asdasd@Asdasds-MacBook-Pro.local>
2024-09-11 22:35:59 +0200

    Rebuilt and update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,30 +55,30 @@ android {
 
 dependencies {
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-    val navVersion = "2.7.7"
+    val navVersion = "2.8.0"
     val appcompatVersion = "1.7.0"
     val roomVersion = "2.6.1"
     implementation("androidx.appcompat:appcompat:$appcompatVersion")
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
-    implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
+    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation(platform("androidx.compose:compose-bom:2024.09.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+    implementation("com.google.dagger:hilt-android:2.52")
+    kapt("com.google.dagger:hilt-android-compiler:2.52")
     implementation("androidx.hilt:hilt-work:1.2.0")
     kapt("androidx.hilt:hilt-compiler:1.2.0")
-    implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation("androidx.work:work-runtime-ktx:2.9.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     // compose-navigation
     implementation("androidx.navigation:navigation-compose:$navVersion")
 
-    implementation("androidx.compose.material:material:1.6.8")
+    implementation("androidx.compose.material:material:1.7.1")
 
 
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
